41cc2bba5f26166269f4e926a44b98b597fc5e62
habioye <78454514+habioye@users.noreply.github.com>
2024-06-08 19:59:44 -0400

    Added LeaderBoard

--- src/main/java/LeaderBoard.java
+++ src/main/java/LeaderBoard.java
@@ -1,4 +1,10 @@
 public class LeaderBoard {
+    QuizApplication.Difficulty d;
     String name; // name of player
     int score; // score of player
+    LeaderBoard(QuizApplication.Difficulty d, String name, int score) {
+        this.d = d;
+        this.name = name;
+        this.score = score;
+    }
 }
