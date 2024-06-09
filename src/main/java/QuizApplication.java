a24e21c98f830cdb4351d9f5ada91d1bc965fe45
habioye <78454514+habioye@users.noreply.github.com>
2024-06-08 20:00:23 -0400

    Added the display for leaderbaord depending on the current or chosen difficulty

--- src/main/java/QuizApplication.java
+++ src/main/java/QuizApplication.java
@@ -133,6 +133,102 @@ public class QuizApplication {
 
 
     }
+    public static String getTable(Difficulty d) {
+        String table;
+        switch (d) {
+            case Difficulty.Easy -> {
+                table = "easy";
+            }
+            case Difficulty.Medium -> {
+                table = "medium";
+            }
+            default -> {
+                table = "hard";
+            }
+        }
+        return table;
+    }
+
+    public static String getLeaderBoardTable(Difficulty d) {
+        String table;
+        switch (d) {
+            case Difficulty.Easy -> {
+                table = "easyleaderboard";
+            }
+            case Difficulty.Medium -> {
+                table = "mediumleaderboard";
+            }
+            default -> {
+                table = "hardleaderboard";
+            }
+        }
+        return table;
+    }
+    public static void insertLeaderBoard(LeaderBoard leaderBoard) {
+        String table = getLeaderBoardTable(leaderBoard.d);
+
+    try(Connection connection = DriverManager.getConnection(url,username,password)) {
+        String createQuery = "INSERT INTO "+table+" (name, score) VALUES (?, ?)";
+        try (PreparedStatement createStatement = connection.prepareStatement(createQuery)){
+            createStatement.setString(1,leaderBoard.name);
+            createStatement.setInt(2,leaderBoard.score);
+            createStatement.executeUpdate();
+            System.out.println("Leaderboard Record Created Successfully");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    }
+    public static ArrayList<LeaderBoard> getLeaderBoard(Difficulty d) {
+        ArrayList<LeaderBoard> ret = new ArrayList<>();
+        LeaderBoard l;
+        String table = getLeaderBoardTable(d);
+        String name;
+        int score;
+        try (Connection connection = DriverManager.getConnection(url,username,password)) {
+            String readQuery = "SELECT name, score FROM "+table+" ORDER BY score DESC";
+            try(Statement readStatment = connection.createStatement();
+            ResultSet resultSet = readStatment.executeQuery(readQuery)) {
+                while (resultSet.next()) {
+                    name = resultSet.getString("name");
+                    score = resultSet.getInt("score");
+
+                    l = new LeaderBoard(d,name,score);
+                    ret.add(l);
+
+                }
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    public static void displayLeaderBoard(Difficulty d) {
+        ArrayList<LeaderBoard> arr = getLeaderBoard(d);
+        String table = getTable(d);
+        // Print header
+        System.out.println("+---------------------+------------+");
+        System.out.println("| " + String.format("%-33s", getTable(d).toUpperCase()+" " + "LEADERBOARD") + " |");
+
+        System.out.println("+---------------------+------------+");
+    System.out.println("| " + String.format("%-20s", "Name") + " | " + String.format("%-10s", "Score") + " |");
+        System.out.println("+---------------------+------------+");
+
+        // Print each entry in the leaderboard
+        for (LeaderBoard l : arr) {
+            String name = l.name;
+            int score = l.score;
+            System.out.println("| " + String.format("%-20s", name) + " | " + String.format("%-10d", score) + " |");
+        }
+
+        // Print footer
+        System.out.println("+---------------------+------------+");
+    }
+
     public static void RunGame() {
 
         Difficulty d;
@@ -140,8 +236,9 @@ public class QuizApplication {
         Scanner scanner = new Scanner(System.in);
         String check;
         int choice;
-        boolean invalid;
+//        boolean invalid;
         int score = 0;
+        String name;
         while (true) {
             System.out.println("""
                     Select your difficulty
@@ -150,7 +247,7 @@ public class QuizApplication {
                     3. Hard
                     """);
             choice = scanner.nextInt();
-            invalid = false;
+//            invalid = false;
             switch (choice) {
                 case 1 -> {
                     d = Difficulty.Easy;
@@ -176,8 +273,8 @@ public class QuizApplication {
                 System.out.println("---".repeat(5));
 
 
-                check = scanner.nextLine();
-                if (check.equals(q.answer)) {
+                check = scanner.nextLine().toLowerCase();
+                if (check.equals(q.answerLower)) {
                     System.out.println("You got the question right");
                     score += 1;
                 } else {
@@ -186,10 +283,53 @@ public class QuizApplication {
                 }
             }
             System.out.println("You have finished the quiz. You score is " + score + " out of 10!");
+            System.out.println("Enter your name for the leaderboard");
+            while (true) {
+                name = scanner.nextLine();
+                if (name.isBlank() || name.isEmpty()) {
+                    System.out.println("Invalid Name. Make sure to insert your name");
+                } else break;
+            }
+            LeaderBoard leaderBoard = new LeaderBoard(d,name,score);
+            insertLeaderBoard(leaderBoard);
+            displayLeaderBoard(d);
+            return;
+
+
         }
     }
     public static void ViewLeaderBoard() {
+        System.out.println("Which Leaderboard do you want to view");
+        int choice;
+        Scanner scanner = new Scanner(System.in);
+        Difficulty d;
+        while (true) {
+            System.out.println("""
+                    1. Easy
+                    2. Medium
+                    3. Hard
+                    """);
+            choice = scanner.nextInt();
 
+            switch (choice) {
+                case 1-> {
+                    d = Difficulty.Easy;
+                }
+                case 2 -> {
+                    d = Difficulty.Medium;
+                }
+                case 3 -> {
+                    d = Difficulty.Hard;
+                }
+                default -> {
+                    System.out.println("You chose an invalid option");
+                    continue;
+                }
+
+            }
+            displayLeaderBoard(d);
+            return;
+        }
     }
     public static void displayDirections() {
         System.out.println("""
@@ -204,7 +344,7 @@ public class QuizApplication {
                 1.) Start Game
                 2.) View LeaderBoard
                 3.) View Directions
-                3.) Exit
+                4.) Exit
                 """);
     }
 
@@ -248,6 +388,7 @@ public class QuizApplication {
                     System.out.println("You selected an invalid option!");
                 }
             }
+
 
 
 
