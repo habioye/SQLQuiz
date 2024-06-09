d8ce578853b901dc38af0cdc79cde3b7d9b78bad
habioye <78454514+habioye@users.noreply.github.com>
2024-06-08 20:02:44 -0400

    Removed the unused code and commented out code

--- src/main/java/QuizApplication.java
+++ src/main/java/QuizApplication.java
@@ -8,86 +8,6 @@ public class QuizApplication {
     public static String url = "jdbc:mysql://localhost:3306/sqlquizdb";
 
 
-    // Creates a table if it does not exist.
-    public static void CreateTable() {
-
-        try (Connection connection = DriverManager.getConnection(url, username, password);
-             Statement statement = connection.createStatement()) {
-
-            String createTableSQL = "CREATE TABLE IF NOT EXISTS Questions (" +
-                    "id INT PRIMARY KEY AUTO_INCREMENT," +
-                    "question VARCHAR(255) NOT NULL UNIQUE," +
-                    "answer VARCHAR(255) NOT NULL" +
-                    ")";
-            statement.execute(createTableSQL);
-            System.out.println("Table created successfully.");
-
-        } catch (SQLException e) {
-            System.err.println("Error creating table: " + e.getMessage());
-        }
-    }
-
-    // Adds the quiz questions and answers from the array list to the SQL database
-    public static void AddQuizQuestions(ArrayList<Quiz> arr) {
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            for (var quiz : arr) {
-                // Create operation
-                String createQuery = "INSERT INTO Questions (question, answer) VALUES ( ?, ?)";
-                try (PreparedStatement createStatement = connection.prepareStatement(createQuery)) {
-//                createStatement.setInt(1, 2);
-                    createStatement.setString(1, quiz.question);
-                    createStatement.setString(2, quiz.answer);
-                    createStatement.executeUpdate();
-                    System.out.println("Record created successfully");
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Deletes the table if it exists
-    public static void DeleteTable() {
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {// Delete operation
-            String deleteTable = "DROP TABLE IF EXISTS Questions";
-            try (PreparedStatement deleteStatement = connection.prepareStatement(deleteTable)) {
-                deleteStatement.executeUpdate();
-                System.out.println("Record deleted successfully");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Gets the quiz question and answer based on the index.
-    public static Quiz getQuiz(int i) {
-        Quiz ret = new Quiz();
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-
-            // Read operation
-            String readQuery = "SELECT question, answer FROM Questions WHERE id = " + i;
-            try (Statement readStatement = connection.createStatement();
-                 ResultSet resultSet = readStatement.executeQuery(readQuery)) {
-                while (resultSet.next()) {
-                    ret.question = resultSet.getString("question");
-                    ret.answer = resultSet.getString("answer");
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return ret;
-
-
-    }
     enum Difficulty {
         Easy,
         Medium,
@@ -236,7 +156,6 @@ public class QuizApplication {
         Scanner scanner = new Scanner(System.in);
         String check;
         int choice;
-//        boolean invalid;
         int score = 0;
         String name;
         while (true) {
@@ -247,7 +166,6 @@ public class QuizApplication {
                     3. Hard
                     """);
             choice = scanner.nextInt();
-//            invalid = false;
             switch (choice) {
                 case 1 -> {
                     d = Difficulty.Easy;
@@ -269,7 +187,6 @@ public class QuizApplication {
                 q = arr.get(i - 1);
                 System.out.println("For question " + i + ":");
                 System.out.println(q.question);
-//                System.out.println(q.answer);
                 System.out.println("---".repeat(5));
 
 
@@ -394,28 +311,6 @@ public class QuizApplication {
 
         }
 
-//
-//        for (int i = 1; i <= arr.size(); i++) {
-//            q = getQuiz(i);
-//            System.out.println("For question "+i+":");
-//            System.out.println(q.question);
-//            System.out.println(q.answer);
-//            System.out.println("---".repeat(5));
-//
-//
-//            check = scanner.nextLine();
-//            if (check.equals(q.answer)) {
-//                System.out.println("You got the question right");
-//                score+=1;
-//            } else {
-//                System.out.println("You got the question wrong. The actual answer is:");
-//                System.out.println(q.answer);
-//            }
-//
-//
-//        }
-//
-//        System.out.println("You have finished the quiz. You score is "+score+" out of 10!");
 
 
     }
